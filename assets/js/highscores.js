912b697547174392a80ec8e779e6eb09c134fe0a
anahita83radfar <anahita83radfar@gmail.com>
2022-01-01 18:43:30 +0000

    Add template literal of high scores list into the unordered list on dom

---
+++ assets/js/highscores.js
@@ -0,0 +1,9 @@
+const higherScoresList = document.querySelector("#higher-scores-list");
+const higherScores = JSON.parse(localStorage.getItem('higherScores')) || [] ;
+
+console.log(higherScores);
+
+higherScoresList.innerHTML = higherScores
+.map(userScore => {
+    return `<li class="list">${userScore.name}: ${userScore.score}</li>`
+}).join("")
